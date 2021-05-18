24992679a62a3b3a1e0412cc0098186b1126f051
Maxim <joodiphone@gmail.com>
2021-05-18 17:26:27 +0300

    dz 11 web 30 laravel

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -8,10 +8,13 @@
     <div class="col-md-8">
         <h1 class="my-4">OUR CONTACTS</h1>
         <ul>
-            <li>viber</li>
-            <li>telegram</li>
-            <li>email</li>
+
+        @foreach($contacts as $contact)
+                <li>{{$contact->name}} : {{$contact->address}}</li>
+            @endforeach
+
         </ul>
     </div>
+
 
 @endsection
