e2b6ec31e34376b65410761f44ac2637bcd10a2c
Maxim <joodiphone@gmail.com>
2021-06-05 19:33:50 +0300

    dz 11 web 30 laravel

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -14,7 +14,7 @@
             @endforeach
 
         </ul>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2747.066831310967!2d30.725604515792252!3d46.48698907263429!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c631965dfc29b9%3A0xcb38625fe2099a1e!2z0JrQvtC80L_RjNGO0YLQtdGA0L3QsNGPINCQ0LrQsNC00LXQvNC40Y8g0KjQkNCT!5e0!3m2!1sru!2sua!4v1622909897332!5m2!1sru!2sua" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
-
 
 @endsection
